09eac32e105c35c70533e86402a6fcb30f326962
takahashi.eisuke <takahashi.eisuke@mdpr.jp>
2025-07-06 17:10:18 +0900

    モックの箇所を修正

--- usecase/repository/mock/product.go
+++ usecase/repository/mock/product.go
@@ -13,13 +13,22 @@ type ProductMock struct {
 }
 
 func (m *ProductMock) Save(ctx context.Context) error {
+	if m.SaveFunc != nil {
+		return m.SaveFunc(ctx)
+	}
 	return nil
 }
 
 func (m *ProductMock) FindAll(ctx context.Context) ([]*entity.Product, error) {
+	if m.FindAllFunc != nil {
+		return m.FindAllFunc(ctx)
+	}
 	return nil, nil
 }
 
 func (m *ProductMock) FindByID(ctx context.Context, productID int64) (*entity.Product, error) {
+	if m.FindByIDFunc != nil {
+		return m.FindByIDFunc(ctx, productID)
+	}
 	return nil, nil
 }
